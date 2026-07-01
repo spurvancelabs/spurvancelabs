514618d2ba63b496dab248d16608d85d39d8ee66
Muhammad Abdullah <abdullahsurnamea786@gmail.com>
2026-07-01 12:23:45 +0500

    redirect / to /landing, switch to pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import React from 'react';
-import "../global.css"
+import { redirect } from "next/navigation";
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is the main page of my Next.js app.</p>
-      <p>Click on the links in the navigation to explore more.</p>
-      <nav>
-        <ul>
-          <li><a href="/signup">Sign Up</a></li>
-          <li><a href="/login">Login</a></li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
-
-export default HomePage;
+export default function HomePage() {
+  redirect("/landing");
+}
